07f4126492ef164f46a7983e85c2c96941d9f5fb
Yan <yanpd01@qq.com>
2021-06-09 17:54:04 +0800

    Version: 0.0.1.2

--- R/var_tools.R
+++ R/var_tools.R
@@ -1,46 +1,64 @@
 #' Var tools
 #'
 #' Move the var to a new specified list.
-#' @param ls ls(); ls(pattern = "aa")
+#'
+#' @param ... vars.
+#' @param names character, the names of vars. eg: ls(pattern = "aa").
+#' @param rm logical, whether to delete the variable.
 #'
 #' @return new list
 #'
 #' @examples
 #' a01 <- 1
 #' a02 <- 2
-#' a_all <- yylst(ls(pattern = "a0"))
+#' b03 <- 1
+#' all <- lst_sum(b03, names=ls(pattern = "a0"))
 #'
 #' @export
-yylst <- function(ls) {
+lst_sum <- function(..., names = NULL, rm = TRUE) {
+    dots <- match.call(expand.dots = FALSE)$...
+    if (length(dots) &&
+        !all(vapply(dots, function(x) is.symbol(x), NA, USE.NAMES = FALSE)))
+        stop("... can`t be character strings")
+    name <- vapply(dots, as.character, "")
+    all <- c(name, names)
     out_list <- list()
-    for ( i in ls) {
+    all <- unique(all)
+    for (i in all) {
         i_wd <- get(i, envir = globalenv())
         out_list <- c(list(i_wd), out_list)
         names(out_list)[1] <- i
         o_wd <- out_list[[i]]
-        if (identical(i_wd, o_wd))
-            rm(list = i, envir = globalenv() )
+        if (rm && identical(i_wd, o_wd))
+            rm(list = i, envir = globalenv())
     }
     return(out_list)
 }
 
 #' Rename var
 #'
-#' @param old_var_name The name of the old variable.
-#' @param new_var_name The name of the new variable.
+#' @param ... The old variable and new variable. Only two items are allowed.
 #'
 #' @return no return
 #'
 #' @examples
 #' a=1
-#' rename_var("a", "c")
+#' rename_var(a, c)
 #' c
 #'
 #' @export
-rename_var <- function(old_var_name, new_var_name) {
+rename_var <- function(...) {
+    dots <- match.call(expand.dots = FALSE)$...
+    if (length(dots) != 2 ||
+        !all(vapply(dots, function(x) is.symbol(x), NA, USE.NAMES = FALSE)))
+        stop("... can`t be character string,
+             and only two items are allowed.")
+    names <- vapply(dots, as.character, "")
+    old_name <- names[1]
+    new_name <- names[2]
     env <- globalenv()
-    wd <- get(old_var_name, envir = env)
-    env[[new_var_name]] <- wd
-    rm(list = old_var_name, envir = env)
+    wd <- get(old_name, envir = env)
+    rm(list = old_name, envir = env)
+    env[[new_name]] <- wd
     invisible()
 }
